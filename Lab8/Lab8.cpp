a37dd9381d2533afea831a10de5212ddd7131651
Tinghui Cheng <tinghui@Tinghuide-MacBook-Pro-1140.local>
2023-06-05 01:15:34 +0800

    lab08

---
+++ Lab8/Lab8.cpp
@@ -0,0 +1,66 @@
+/*
+ * GccApplication3.cpp
+ *
+ * Created: 2019/4/22 下午 03:50:13
+ * Author : USER
+ */ 
+#define F_CPU 1000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+void str1(){
+	OCR0A=200;
+	OCR0B=0;
+	OCR2A=200;
+	OCR2B=0;
+	TCCR0A=0b10100011; // fast PWM, non-inverted
+	TCCR0B=0b00000010; // timer prescaler
+	TCCR2A=0b10100011; // fast PWM, non-inverted
+	TCCR2B=0b00000010; // timer prescaler
+	_delay_ms(2000);
+}
+void str2(){
+	OCR0A=200;
+	OCR0B=0;
+	OCR2A=200;
+	OCR2B=0;
+	TCCR0A=0b10100011; // fast PWM, non-inverted
+	TCCR0B=0b00000010; // timer prescaler
+	TCCR2A=0b10100011; // fast PWM, non-inverted
+	TCCR2B=0b00000010; // timer prescaler
+	_delay_ms(4000);
+}
+void left(){
+	OCR0A=255;
+	OCR0B=0;
+	OCR2A=100;
+	OCR2B=0;
+	TCCR0A=0b10100011; // fast PWM, non-inverted
+	TCCR0B=0b00000010; // timer prescaler
+	TCCR2A=0b10100011; // fast PWM, non-inverted
+	TCCR2B=0b00000010; // timer prescaler
+	_delay_ms(1900);
+}
+int main(void)
+{
+	CLKPR=(1<<CLKPCE);
+	CLKPR=0b00000011; // set clk to 1Mhz
+	DDRD=0b11111111;
+	DDRB=0b11111111;
+	//OCR0A=200;
+	//OCR0B=0;
+//	OCR2A=200;
+//	OCR2B=0;
+	while(1)
+	{
+		str1();
+		left();
+		str2();
+		left();
+	}
+	
+	
+	
+	
+}
+
